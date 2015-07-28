a3a3e116781fd8e06c3e640b744f148bf13537c3
RobertBowie <robertb1903@gmail.com>
2015-07-27 21:20:24 -0700

    Messes with tie_flag and the_game() + notes
    Some notes to pursue in the morning.

--- TTT_clean.js
+++ TTT_clean.js
@@ -1,22 +1,20 @@
 var board = [["_","_","_"],["_","_","_"],["_","_","_"]];
 var new_game = function(){
-    board = [["_","_","_"],["_","_","_"],["_","_","_"]];
-    tie_flag = true;
+    this.board = [["_","_","_"],["_","_","_"],["_","_","_"]];
 };
 
 var print_board = function(game){
     console.log(game[0] + "\n" + game[1] + "\n" + game[2]);
 };
 
-var tie_flag = false;
 
 var make_move = function(row, column, x_o){
   board[row][column] = x_o;
 };
-
+var win_flag = false;
 var on_win = function(player){
-    tie_flag = false;
     console.log(player + "'s win!");
+    win_flag = true;
 };
 
 var cats_game = function(){
@@ -42,17 +40,25 @@ var win_check = function(board){
     set_check(board[2][0], board[2][1], board[2][2]);
     set_check(board[0][0], board[1][1], board[2][2]);
     set_check(board[0][2], board[1][1], board[2][0]);
-    if(tie_flag === true){
-        cats_game();
-    }
 };
+
+//experimenting with for loop.  Prefer the idea of using the win_check to determine game runtime
+//try to implement a less intensive check for win and tie it into the games run cycle.
 var the_game = function(){
   new_game();
   print_board(board);
-  var x_o = window.prompt("Would you like to be X's or O's?", "X");
+  var x_o = window.prompt("X's or O's?", "X");
   var row = window.prompt("What row?");
   var col = window.prompt("What column?");
   make_move(row, col, x_o);
+  for(var i = 0; i < 9; i++){
   print_board(board);
+  x_o = window.prompt("X's or O's?", "X");
+  row = window.prompt("What row?");
+  col = window.prompt("What column?");
+  make_move(row, col, x_o);
+  win_check(board);
+  }
+  
 };
 the_game();
