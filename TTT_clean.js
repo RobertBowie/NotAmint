733eaa05615cae4ef4c908e53049a79a31f560a5
RobertBowie <robertb1903@gmail.com>
2015-07-28 10:39:02 -0700

    Works
    Rough working version of Tic Tac Toe.  Knows when X's or O's have 3 in a
    row or all 9 moves have been made without a winner.

--- TTT_clean.js
+++ TTT_clean.js
@@ -14,7 +14,7 @@ var make_move = function(row, column, x_o){
 var win_flag = false;
 var on_win = function(player){
     console.log(player + "'s win!");
-    win_flag = true;
+    return true;
 };
 
 var cats_game = function(){
@@ -25,40 +25,50 @@ var set_check = function(first, sec, third){
     var X = "X";
     var O = "O";
     if(first === X && sec === X && third === X){
-        on_win(X);
+        return on_win(X);
     } else if (first === O && sec === O && third === O){
-        on_win(O);
+        return on_win(O);
     }
 };
 
 var win_check = function(board){
-    set_check(board[0][0], board[0][1], board[0][2]);
-    set_check(board[1][0], board[1][1], board[1][2]);
-    set_check(board[2][0], board[2][1], board[2][2]);
-    set_check(board[0][0], board[1][0], board[2][0]);
-    set_check(board[1][0], board[1][1], board[1][2]);
-    set_check(board[2][0], board[2][1], board[2][2]);
-    set_check(board[0][0], board[1][1], board[2][2]);
-    set_check(board[0][2], board[1][1], board[2][0]);
+    if(set_check(board[0][0], board[0][1], board[0][2])){
+        return true;
+    }else if(set_check(board[1][0], board[1][1], board[1][2])){
+        return true;
+    }else if(set_check(board[2][0], board[2][1], board[2][2])){
+        return true;
+    }else if(set_check(board[0][0], board[1][0], board[2][0])){
+        return true;
+    } else if(set_check(board[1][0], board[1][1], board[1][2])){
+        return true;
+    }else if(set_check(board[2][0], board[2][1], board[2][2])){
+        return true;
+    }else if(set_check(board[0][0], board[1][1], board[2][2])){
+        return true;
+    }else if(set_check(board[0][2], board[1][1], board[2][0])){
+        return true;
+    }
 };
 
 //experimenting with for loop.  Prefer the idea of using the win_check to determine game runtime
 //try to implement a less intensive check for win and tie it into the games run cycle.
 var the_game = function(){
+  var x_o;  var row;  var col;
   new_game();
   print_board(board);
-  var x_o = window.prompt("X's or O's?", "X");
-  var row = window.prompt("What row?");
-  var col = window.prompt("What column?");
-  make_move(row, col, x_o);
-  for(var i = 0; i < 9; i++){
-  print_board(board);
-  x_o = window.prompt("X's or O's?", "X");
-  row = window.prompt("What row?");
-  col = window.prompt("What column?");
-  make_move(row, col, x_o);
-  win_check(board);
+//need to repeat until EITHER a win or board is full
+  for(var i = 1; i < 10; i++){
+    x_o = window.prompt("X's or O's?", "X");
+    row = window.prompt("What row?");
+    col = window.prompt("What column?");
+    make_move(row, col, x_o);
+    print_board(board);
+    if(win_check(board)){
+      break;
+    } else if(i === 9){
+      cats_game();
+    }
   }
-  
 };
 the_game();
