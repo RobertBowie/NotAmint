d40bbd049be3d08383366562f0c36f0e5a29a0ec
RobertBowie <robertb1903@gmail.com>
2015-07-28 20:45:37 -0700

    Moves around functions and variables
    Makes random_empty_space account for game in progress.

--- TTT_clean.js
+++ TTT_clean.js
@@ -53,36 +53,33 @@ var win_check = function(){
 
 
 
-var random_empty_space = function() { //returns coordinates of random blank in board
-  var emptys = [];
-  for(var i in board_squares){
-    if(board_squares[i][0] === "_"){
-      emptys.push(board_squares[i][1]);
-    }
-    console.log(emptys);
-  }
-  return emptys[Math.floor(Math.random() * emptys.length)];
-};
 
-var board_squares = [[board[0][0], [0,0]], [board[0][1], [0,1]], [board[0][2], [0,2]], [board[1][0], [1,0]],
-        [board[1][1], [1,1]], [board[1][2], [1,2]], [board[2][0], [2,0]], [board[2][1], [2,1]], [board[2][2], [2,2]]];
-
-var ai_row = 0, ai_col = 0;
-var splitter = function(coords){
-    ai_row = coords[0];
-    ai_col = coords[1];
-    
-};
-var simple_ai_move = function(){
-  /*place an O in random_empty_space*/
-  splitter(random_empty_space());
-  make_move(ai_row, ai_col, "O");
-};
 
 var the_game = function(){
   var x_o;  var row;  var col;
   new_game();
   print_board();
+  var ai_row = 0, ai_col = 0;
+  //took board_squares out of here
+  var splitter = function(coords){
+    ai_row = coords[0];
+    ai_col = coords[1];
+  };
+  var random_empty_space = function() { //returns coordinates of random blank in board
+    var emptys = [];
+    for(var i in board_squares){
+      if(board_squares[i][0] === "_"){
+        emptys.push(board_squares[i][1]);
+      }
+    console.log(emptys);
+    }
+  return emptys[Math.floor(Math.random() * emptys.length)];
+  };
+  var simple_ai_move = function(){
+  /*place an O in random_empty_space*/
+  splitter(random_empty_space());
+  make_move(ai_row, ai_col, "O");
+  };
 //repeat until either a win or full board
   for(var i = 1; i < 10; i++){
     x_o = "X";
@@ -98,6 +95,11 @@ var the_game = function(){
       col = window.prompt("What column?");
     }
     make_move(row, col, x_o);
+    var board_squares = [[board[0][0], [0,0]], [board[0][1], [0,1]],
+                       [board[0][2], [0,2]], [board[1][0], [1,0]],
+                       [board[1][1], [1,1]], [board[1][2], [1,2]],
+                       [board[2][0], [2,0]], [board[2][1], [2,1]],
+                       [board[2][2], [2,2]]];
     print_board();
     simple_ai_move();
     print_board();
@@ -116,6 +118,3 @@ would lead to more games ending in a draw than the first plan but would be beata
 that behavior.  The final, most complex and winning, strategy would involve having a specific plan in response to the
 board setup.  This AI could be designed to always go second (probably less complex than an offensive AI) and would end
 almost every game in a draw. */
-
-
-//most, if not all of this will have to be inside the_game() possibly
